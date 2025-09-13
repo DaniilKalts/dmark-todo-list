5d306343a453ad8af2505554058fb78fcb8f7954
DaniilKalts <kaltsdaniil@gmail.com>
2025-09-13 07:17:59 +0500

    build(client): add Prettier config and Tailwind CSS setup

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 
 function App() {
-    return (
-        <div id="App">
-            <p>To-Do клиентскую часть...</p>
-        </div>
-    )
+  return (
+    <div id="App">
+      <p>To-Do клиентскую часть...</p>
+      <p className="text-red-500">TailwindCSS на месте :D</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
